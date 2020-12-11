e9a9ddf87f8985aad861694f56fb98bd64f97751
mohammedrazakhan <rk6265766@gmail.com>
2020-12-11 22:46:34 +0530

    commit

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-//const mongoose = require('mongoose')
-const morgan = require('morgan')
 const express = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
 const connectDB = require('./config/db')
 const passport = require('passport')
 const bodyParser = require('body-parser')
 const routes = require('./routes/index')
 
-
-connectDB();
+connectDB()
 
 const app = express()
 
@@ -21,7 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(routes)
 app.use(passport.initialize())
-require('../Plutter-Server/config/passport')(passport)
-const PORT = process.env.PORT || 3000;
+require('./config/passport')(passport)
+
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
